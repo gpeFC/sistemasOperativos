e8439ec26ad3446fe977cf8f0e5d36a1935219dc
EmanuelGP <emanuel.garp@gmail.com>
2015-05-07 09:00:49 -0500

    Actualizacion-simulador(v2).

--- proyectoFinal/codigos/simulador_v2.py
+++ proyectoFinal/codigos/simulador_v2.py
@@ -3,6 +3,7 @@ Version 2 del simulador.
 """
 
 import random
+import math
 import time
 import os
 
@@ -43,107 +44,94 @@ class Simulador:
 				self.mapa[x][y] = self.simbolos[1]
 				cntdr += 1
 
-	def movimiento_autonomo(self):
-		""" Documentacion para la funcion <movimiento_autonomo>. """
-		if self.orientacion == 'N':
-			if 0<=self.axy[0]-1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]-1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-			elif 0<=self.axy[0]<self.filas and 0<=self.axy[1]+1<self.columnas and self.mapa[self.axy[0]][self.axy[1]+1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'E'
-			elif 0<=self.axy[0]<self.filas and 0<=self.axy[1]-1<self.columnas and self.mapa[self.axy[0]][self.axy[1]-1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'O'
-			elif 0<=self.axy[0]+1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]+1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'S'
-		elif self.orientacion == 'E':
-			if 0<=self.axy[0]<self.filas and 0<=self.axy[1]+1<self.columnas and self.mapa[self.axy[0]][self.axy[1]+1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-			elif 0<=self.axy[0]+1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]+1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'S'
-			elif 0<=self.axy[0]-1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]-1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'N'
-			elif 0<=self.axy[0]<self.filas and 0<=self.axy[1]-1<self.columnas and self.mapa[self.axy[0]][self.axy[1]-1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'O'
-		elif self.orientacion == 'O':
-			if 0<=self.axy[0]<self.filas and 0<=self.axy[1]-1<self.columnas and self.mapa[self.axy[0]][self.axy[1]-1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-			elif 0<=self.axy[0]-1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]-1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'N'
-			elif 0<=self.axy[0]+1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]+1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'S'
-			elif 0<=self.axy[0]<self.filas and 0<=self.axy[1]+1<self.columnas and self.mapa[self.axy[0]][self.axy[1]+1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'E'
-		elif self.orientacion == 'S':
-			if 0<=self.axy[0]+1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]+1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-			elif 0<=self.axy[0]<self.filas and 0<=self.axy[1]-1<self.columnas and self.mapa[self.axy[0]][self.axy[1]-1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'O'
-			elif 0<=self.axy[0]<self.filas and 0<=self.axy[1]+1<self.columnas and self.mapa[self.axy[0]][self.axy[1]+1]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[1] = self.axy[1]+1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'E'
-			elif 0<=self.axy[0]-1<self.filas and 0<=self.axy[1]<self.columnas and self.mapa[self.axy[0]-1][self.axy[1]]!=self.simbolos[1]:
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-				self.axy[0] = self.axy[0]-1
-				self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-				self.orientacion = 'N'
-
-	def movimiento_guiado(self, mov):
-		""" Documentacion para la funcion <movimiento_guiado>. """
-		if mov == 'N':
+	def movimiento(self, movimientos):
+		""" Documentacion para la funcion <movimiento>. """
+		indice = 0
+		menor = self.distancia(0,self.filas-1,0,self.columnas-1)
+		mxy = self.mxy
+		for i in range(len(movimientos)):
+			if movimientos[i] != None:
+				dxy = movimientos[i]
+				if self.distancia(dxy[0],mxy[0],dxy[1],mxy[1]) < menor:
+					indice = i
+					menor = self.distancia(dxy[0],mxy[0],dxy[1],mxy[1])
+		if indice == 0:
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
 			self.axy[0] = self.axy[0]-1
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-		elif mov == 'E':
+			self.orientacion = 'N'
+		elif indice == 1:
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
+			self.axy[0], self.axy[1] = self.axy[0]-1, self.axy[1]+1
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
+			self.orientacion = 'NE'
+		elif indice == 2:
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
 			self.axy[1] = self.axy[1]+1
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-		elif mov == 'O':
+			self.orientacion = 'E'
+		elif indice == 3:
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
-			self.axy[1] = self.axy[1]-1
+			self.axy[0], self.axy[1] = self.axy[0]+1, self.axy[1]+1
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
-		elif mov == 'S':
+			self.orientacion = 'SE'
+		elif indice == 4:
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
 			self.axy[0] = self.axy[0]+1
 			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
+			self.orientacion = 'S'
+		elif indice == 5:
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
+			self.axy[0], self.axy[1] = self.axy[0]+1, self.axy[1]-1
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
+			self.orientacion = 'SO'
+		elif indice == 6:
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
+			self.axy[1] = self.axy[1]-1
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
+			self.orientacion = 'O'
+		elif indice == 7:
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[0]
+			self.axy[0], self.axy[1] = self.axy[0]-1, self.axy[1]-1
+			self.mapa[self.axy[0]][self.axy[1]] = self.simbolos[2]
+			self.orientacion = 'NO'
+
+	def movimientos(self):
+		""" Documentacion para la funcion <movimientos>. """
+		movimientos = [None] * 8
+		axy = self.axy
+		f, c = self.filas, self.columnas
+		mapa = self.mapa
+		if (0 <= axy[0]-1 < f) and (0 <= axy[1] < c) and \
+		(mapa[axy[0]-1][axy[1]] != self.simbolos[1]):	# N
+			movimientos[0] = [axy[0]-1, axy[1]]
+		if (0 <= axy[0]-1 < f) and (0 <= axy[1]+1 < c) and \
+		(mapa[axy[0]-1][axy[1]+1] != self.simbolos[1]):	# NE
+			movimientos[1] = [axy[0]-1, axy[1]+1]
+		if (0 <= axy[0] < f) and (0 <= axy[1]+1 < c) and \
+		(mapa[axy[0]][axy[1]+1] != self.simbolos[1]):	# E
+			movimientos[2] = [axy[0], axy[1]+1]
+		if (0 <= axy[0]+1 < f) and (0 <= axy[1]+1 < c) and \
+		(mapa[axy[0]+1][axy[1]+1] != self.simbolos[1]):	# SE
+			movimientos[3] = [axy[0]+1, axy[1]+1]
+		if (0 <= axy[0]+1 < f) and (0 <= axy[1] < c) and \
+		(mapa[axy[0]+1][axy[1]] != self.simbolos[1]):	# S
+			movimientos[4] = [axy[0]+1, axy[1]]
+		if (0 <= axy[0]+1 < f) and (0 <= axy[1]-1 < c) and \
+		(mapa[axy[0]-1][axy[1]-1] != self.simbolos[1]):	# SO
+			movimientos[5] = [axy[0]+1, axy[1]-1]
+		if (0 <= axy[0] < f) and (0 <= axy[1]-1 < c) and \
+		(mapa[axy[0]][axy[1]-1] != self.simbolos[1]):	# O
+			movimientos[6] = [axy[0], axy[1]-1]
+		if (0 <= axy[0]-1 < f) and (0 <= axy[1]-1 < c) and \
+		(mapa[axy[0]-1][axy[1]-1] != self.simbolos[1]):	# NO
+			movimientos[7] = [axy[0]-1, axy[1]-1]
+		print movimientos
+		return movimientos
+
+	def distancia(self, x1, x2, y1, y2):
+		""" Documentacion para la funcion <distancia>. """
+		return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
 
 	def mostrar_mapa(self):
 		""" Documentacion para la funcion <mostrar_mapa>. """
@@ -154,23 +142,17 @@ class Simulador:
 
 
 if __name__ == '__main__':
-	simulador = Simulador(10,10," ",".")
+	simulador = Simulador(27,18," ",".")
 	simulador.poblar_mapa()
-	control = True
-	#control = False
 	contador = 0
 	while True:
 		contador += 1
 		os.system('clear')
 		simulador.mostrar_mapa()
-		if control:
-			simulador.movimiento_autonomo()
-			time.sleep(1)
-		else:
-			movimiento = raw_input("(movimiento) ")
-			movimiento = movimiento.upper()
-			simulador.movimiento_guiado(movimiento)
+		print simulador.axy
+		simulador.movimiento(simulador.movimientos())
 		if simulador.axy == simulador.mxy:
-			print "Meta alcanzada"
+			print "Meta alcanzada..."
 			break
-	print contador
+		time.sleep(1)
+	print "Movimientos:", contador
